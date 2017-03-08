344cfad3c64e5d4ad83c836da4455b6b1cbfe9c7
Saranda Sadiku <sarandasadiku@live.com>
2017-03-08 07:58:27 -0800

    #8

---
+++ JavaScript/Creating_Objects_I.js
@@ -0,0 +1,32 @@
+function VehicleConstructor(name, wheels, passengers){
+    var  vehicle = {};
+
+    vehicle.name = name;
+    vehicle.wheels = wheels;
+    vehicle.passengers = passengers;
+
+
+    vehicle.makeNoise = function(){
+        console.log("Makeing noise!");
+    }
+    return vehicle;
+}
+var Bike = VehicleConstructor("Bike", 2, 1);
+// Bike.makeNoise();
+Bike.makeNoise = function(){
+    console.log("ring ring");
+}
+// Bike.makeNoise();
+var Sedan = VehicleConstructor("Sedan", 4, 4);
+Sedan.makeNoise = function(){
+    console.log("honk honk");
+}
+Sedan.makeNoise();
+
+var Bus = VehicleConstructor("Bus", 8,1);
+Bus.pickUpPassengers = function(passengers){
+    Bus.passengers += passengers;
+}
+console.log(Bus.passengers)
+Bus.pickUpPassengers(14);
+console.log(Bus.passengers)
